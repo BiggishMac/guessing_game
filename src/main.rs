0a85456e9fa1881b4f8249bf4f1ff8fbbde3f1e9
Ben Mac <benjaminmacdessi@BMM.local>
2024-03-29 12:58:28 +1100

    feat: create a number guessing game
    Start of guessing game

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io::stdin;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    print!("You guessed: {guess}");
+}
